e97abf1645f82aa3652a2d384a356c8d6e160e6a
Branko <branko@ubuntu.(none)>
2013-11-12 21:43:31 -0500

    Finished chapter 2 usa.gov data from bit.ly

--- chapter2.py
+++ chapter2.py
@@ -1,6 +1,6 @@
 import json
 
-path = 'ch02/usagov_bitly_data2012-03-16-1331923249.txt'        # json file
+path = 'usagov_bitly_data2012-03-16-1331923249.txt'        	# json file
 records = [json.loads(line) for line in open(path)]             # list of dictionaries with the json file
 
 time_zones = [rec['tz'] for rec in records if 'tz' in rec]      # take all the time-zones (note that not all records have 'tz')
@@ -43,7 +43,42 @@ print top_counts(time_zones_count)
 # counts = Counter(time_zones)
 # counts.most_common(10)
 
-from pandas import DataFram, Series
+from pandas import DataFrame, Series
 import pandas as pd; import numpy as np
 
 frame = DataFrame(records)
+# printing a frame will give you a summary of the Data Frame
+# attributes in a Data Frame can be accessed by name
+tz = frame['tz']
+# counts of a series can be accessed by using value_counts()
+tz_counts = frame['tz'].value_counts()
+# fillna function can replace missing (NA) values with unknown (empty strings)
+clean_tz = tz.fillna("Missing")
+clean_tz[clean_tz == ""] = "Unknown"
+tz_counts = clean_tz.value_counts()
+
+# plotting
+tz_counts[:10].plot(kind="barh",rot=0)
+
+# create a series based on the first letter of the field 'a'
+results = Series([x.split()[0] for x in frame.a.dropna()])
+
+# exclude records with null values in parameter 'a'
+cframe = frame[frame.a.notnull()]
+operating_system = np.where(cframe['a'].str.contains('Windows'), 'Windows', 'Not Windows')
+
+# pivot on timezone, count of  operating system
+by_tz_os = cframe.groupby(['tz',operating_system])
+agg_counts = by_tz_os.size().unstack().fillna(0)
+# select top overall time zones by constructing an indirect index array from row counts in agg_counts
+indexer = agg_counts.sum(1).argsort()
+
+count_subset = agg_counts.take(indexer)[-10:]
+# plot bar graph of operating system by time zone
+count_subset.plot(kind='barh',stacked=True)
+# plot relative bar graph of operating system by time zone
+normed_subset = count_subset.div(count_subset.sum(1), axis=0)
+normed_subset.plot(kind='barh', stacked=True)
+
+
+
